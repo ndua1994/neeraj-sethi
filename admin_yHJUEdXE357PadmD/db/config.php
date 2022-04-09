0612ff23a1051afb23b35d8c6fa7382d3044a344
ndua1994 <duanishant71@gmail.com>
2022-04-09 17:01:54 +0530

    db changes

--- admin_yHJUEdXE357PadmD/db/config.php
+++ admin_yHJUEdXE357PadmD/db/config.php
@@ -2,12 +2,12 @@
 error_reporting(0);
 session_start();
 date_default_timezone_set('Asia/kolkata');
-define('BASE_URL','http://localhost/neeraj-sethi/admin_yHJUEdXE357PadmD/');
-$dbhost='localhost';
+define('BASE_URL','https://neeraj-sethi.drehomes.ae/admin_yHJUEdXE357PadmD/');
+$dbhost='drehomes_neeraj';
 $dbusername='root';
-$dbpassword='';
-$dbname='db_neeraj_sethi';
-$conn=mysqli_connect('localhost','root','','db_neeraj_sethi');
+$dbpassword='E8CPoHb%@bmE';
+$dbname='drehomes_neeraj_sethi';
+$conn=mysqli_connect('localhost','drehomes_neeraj','E8CPoHb%@bmE','drehomes_neeraj_sethi');
 if(!$conn)
 {
 	die('Database Connection Failed '.mysqli_error());
