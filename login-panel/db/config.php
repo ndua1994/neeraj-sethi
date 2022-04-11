eced6d029935f6fe7d282c343228d95fdc3810f2
ndua1994 <duanishant71@gmail.com>
2022-04-11 17:59:50 +0530

    db configuration

--- login-panel/db/config.php
+++ login-panel/db/config.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 date_default_timezone_set('Asia/kolkata');
-define('BASE_URL','http://localhost/neeraj-sethi/login-panel/');
-define('BASE_URL_MAIN','http://localhost/neeraj-sethi/');
+define('BASE_URL','https://neeraj-sethi.drehomes.ae/login-panel/');
+define('BASE_URL_MAIN','https://neeraj-sethi.drehomes.ae/');
 $dbhost='localhost';
-$dbusername='root';
-$dbpassword='';
-$dbname='db_neeraj_sethi';
-$conn=mysqli_connect('localhost','root','','db_neeraj_sethi');
+$dbusername='drehomes_neeraj';
+$dbpassword='E8CPoHb%@bmE';
+$dbname='drehomes_neeraj_sethi';
+$conn=mysqli_connect('localhost','drehomes_neeraj','E8CPoHb%@bmE','drehomes_neeraj_sethi');
 if(!$conn)
 {
 	die('Database Connection Failed '.mysqli_error());
